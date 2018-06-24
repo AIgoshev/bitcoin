231410105f2f87869f7282e0ced01d7a5d47f0ad
Alexey <alex.igoshev@quickblox.com>
2018-06-24 16:12:40 +0300

    additional logging

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1976,6 +1976,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
                 // to avoid banning same-fork peers who haven't upgraded to the new consensus rules yet (due to them
                 // activating just now), we let them stay as long as we are currently on the fork height
                 if (FORK_BLOCK != chainActive.Tip()->nHeight || (pfrom->fhash != FORK_HASH_UINT256 && pfrom->fhash.ToString() != "e97587903ff4a9ff29d5558a808844b42dec748203e64c178ca7cfc79f4bec1a")) {
+                  LogPrintf("peer from consensus-invalid fork %s, banning\n", pfrom->fhash.ToString().c_str());
                     LogPrintf("%s: BAN_12\n", __func__);
                     Misbehaving(pfrom->GetId(), nDoS);
                 }
